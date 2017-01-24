6dd09363c9dd477139e4c91103df7229458ddd86
bardlegeuse <devancsis@gmail.com>
2017-01-24 08:06:00 -0700

    changed filler obj & updated css

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -12,7 +12,7 @@ class Projects extends Component {
             },
             {
                 "id": 2,
-                "name": "Brain Checklist",
+                "name": "Email Generator",
                 "thumb": "https://placehold.it/150x80?text=IMAGE",
                 "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             },
@@ -21,30 +21,12 @@ class Projects extends Component {
                 "name": "Brain Checklist",
                 "thumb": "https://placehold.it/150x80?text=IMAGE",
                 "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            },
-            {
-                "id": 4,
-                "name": "Brain Checklist",
-                "thumb": "https://placehold.it/150x80?text=IMAGE",
-                "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            },
-            {
-                "id": 5,
-                "name": "Brain Checklist",
-                "thumb": "https://placehold.it/150x80?text=IMAGE",
-                "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            },
-            {
-                "id": 6,
-                "name": "Brain Checklist",
-                "thumb": "https://placehold.it/150x80?text=IMAGE",
-                "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             }
         ]
 
-        var projList = projects.map(function (obj) {
+        var projList = projects.map(function (obj, i) {
             return(
-                <div className="col-md-4 col-sm-6">
+                <div key="{i}" className="col-md-4 col-sm-6">
                 <p>{obj.name}</p>
                 <img src={obj.thumb} className="img-responsive thumb" alt="placeHolder"></img>
                 <p>{obj.about}</p>
@@ -58,11 +40,11 @@ class Projects extends Component {
             < div >
                 <div className="jumbotron">
                     <div className="container text-center">
-                        <h1>My Portfolio</h1>
-                        <p>Some text that represents "Me"...</p>
+                        <h1>Projects</h1>
+                        <p>A Sampling of my best projects</p>
                     </div>
                 </div>
-                <div className="projectBox">
+                <div className=" col-sm-12">
                     {projList}
                 </div>
             </div >
