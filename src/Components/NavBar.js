9ba422e30809a9d9ea020ba56e63f7773a5b60a2
bardlegeuse <devancsis@gmail.com>
2017-02-06 09:04:20 -0700

    fixed navbar for mobile version

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,57 +3,37 @@ import React, { Component } from 'react';
 
 
 class NavBar extends Component {
-    constructor() {
-        super();
-        this.state = {
-            collapsed: true,
-        };
-    }
-
-    toggleCollapse() {
-        const collapsed = !this.state.collapsed;
-        this.setState({ collapsed });
-        console.log(collapsed);
-    }
-
 
     render() {
 
-        const { collapsed } = this.state;
-        const navClass = collapsed ? "collapse" : "";
 
         return (
             <div>
                 <nav className="navbar navbar-inverse">
-                    <div className="container-fluid">
-                        <div className="navbar-header">
-                            <button type="button" className="navbar-toggle" onClick={this.toggleCollapse.bind(this)} >
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-                            </button>
+                    <div className="navBarBox">
+                        <div>
                             <a className="navbar-brand" href="/" title="Home"><div className="d-shadow">
                             <span className="lastinitial">S</span>
                             </div></a>
                         </div>
-                        <div className={"navbar-collapse " + navClass} id="myNavbar">
+                        <div id="myNavbar">
                             <ul className="nav navbar-nav">
                                 <li>
                                     <a href="/projects">
                                         <span className="fa fa-code navIcon"></span>
-                                        <span>Projects</span>
+                                        <span className="navLabel">Projects</span>
                                     </a>
                                 </li>
                                 <li>
                                     <a href="/resume">
                                         <span className="fa fa-id-card-o navIcon"></span>
-                                        <span>Resume</span>
+                                        <span className="navLabel">Resume</span>
                                     </a>
                                 </li>
                                 <li>
                                     <a href="/contact">
                                         <span className="fa fa-envelope-o navIcon"></span>
-                                        <span>Contact Me</span>
+                                        <span className="navLabel">Contact Me</span>
                                     </a>
                                 </li>
                             </ul>
