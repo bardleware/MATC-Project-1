f1d87b86e314b422c8412a3c0f94c4e02d622828
bardlegeuse <devancsis@gmail.com>
2017-03-09 13:03:00 -0700

    added link to tribute page

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -48,10 +48,10 @@ class Projects extends Component {
                 "id": 5,
                 "name": "FCC - Tribute Page",
                 "thumb": "https://placehold.it/150x80?text=IMAGE",
-                "codeLink": "#",
+                "codeLink": "http://codepen.io/bardleware/pen/begyLg",
                 "tryMeIcon":"fa fa-codepen",
                 "seeCodeIcon":"fa fa-codepen",
-                "projectLink": "#",
+                "projectLink": "http://codepen.io/bardleware/pen/begyLg",
                 "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             }
         ]
