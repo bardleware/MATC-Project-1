4a133bb10e3218b293f518cd1d87fc76ce3a788e
bardleware <devan.sisson@vivintsolar.com>
2017-05-22 21:05:26 -0600

    added buttons on the home page for easy nav

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,49 +5,49 @@ import { Link } from 'react-router';
 
 class NavBar extends Component {
 
-    render() {
+  render() {
 
 
-        return (
+    return (
+      <div>
+        <nav className="navbar navbar-inverse">
+          <div className="navBarBox">
             <div>
-                <nav className="navbar navbar-inverse">
-                    <div className="navBarBox">
-                        <div>
-                            <Link to="/" title="Home">
-                                <div className="d-shadow"></div>
-                            </Link>
-                        </div>
-                        <div id="myNavbar">
-                            <ul className="nav navbar-nav">
-                                <li>
-                                    <Link to="/projects">
-                                        <span className="fa fa-code navIcon"></span>
-                                        <span className="navLabel">Projects</span>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="/resume">
-                                        <span className="fa fa-id-card-o navIcon"></span>
-                                        <span className="navLabel">Resume</span>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="/contact">
-                                        <span className="fa fa-envelope-o navIcon"></span>
-                                        <span className="navLabel">Contact Me</span>
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-                <div className="children">
-                    {this.props.children}
-
-                </div>
+              <Link to="/" title="Home">
+                <div className="d-shadow"></div>
+              </Link>
             </div>
-        );
-    }
+            <div id="myNavbar">
+              <ul className="nav navbar-nav">
+                <li>
+                  <Link to="/projects">
+                    <span className="fa fa-code navIcon"></span>
+                    <span className="navLabel">Projects</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/resume">
+                    <span className="fa fa-id-card-o navIcon"></span>
+                    <span className="navLabel">Resume</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/contact">
+                    <span className="fa fa-envelope-o navIcon"></span>
+                    <span className="navLabel">Contact Me</span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="children">
+          {this.props.children}
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default NavBar;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,35 +1,48 @@
 /* eslint-disable */
 import React, { Component } from 'react';
-//import logo from '';
+import { Link } from 'react-router';
 import Skills from '../Components/About-Me/Skills';
 import '../style/home.css';
 
 class Home extends Component {
 
-    //<Skills skills={skills}/>
-    render() {
-        return (
-            <div>
-                <div className="home-header">
-                    <img className="profile-pic" src="http://devansprojects.net/fileserv/profile-pic.jpg" alt="Picture of Devan"></img>
-                    <h3>Hello, my name is Devan.</h3>
-                </div>
-                <div className="paraContainer">
-                    <div  className="paraText">
-                        <p>I am a full stack web developer currently attending a Web developer course at MATC in Lehi, UT where I am fine tuning and learning more about web development.</p>
+  render() {
+    return (
+      <div>
+        <div className="home-header">
+          <img className="profile-pic" src="http://devansprojects.net/fileserv/profile-pic.jpg" alt="Picture of Devan"></img>
+          <h3>Hello, my name is Devan.</h3>
+        </div>
+        <div className="paraContainer">
+          <div className="paraText">
+            <p>I am a full stack web developer currently attending a Web developer course at MATC in Lehi, UT where I am fine tuning and learning more about web development.</p>
 
-                        <p>I have been working on learning how to code and program in my free time with plans to return to college to earn a degree in Software Development from Western Governor's University.</p>
-                    </div>
-
-                </div>
-
-
-
+            <p>I have been working on learning how to code and program in my free time with plans to return to college to earn a degree in Software Development from Western Governor's University.</p>
+          </div>
+        </div>
+        <div className="navRow">
+          <Link to="/projects">
+            <div className="navButton">
+              <div className="fa fa-code"></div>
+              <div className="navLabel">Projects</div>
             </div>
+          </Link>
+          <Link to="/resume">
+            <div className="navButton">
+              <div className="fa fa-id-card-o"></div>
+              <div className="navLabel">Resume</div>
+            </div>
+          </Link>
+        </div>
 
 
-        );
-    }
+
+
+      </div>
+
+
+    );
+  }
 }
 
 export default Home;
